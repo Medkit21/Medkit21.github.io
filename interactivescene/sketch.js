ef04f1bca69ce3da1b51a92c177f7ae29e2a1938
Strongarm2801204 <2801204@sps-student.sk.ca>
2019-09-16 14:13:37 -0600

    yee

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -7,43 +7,61 @@
 
 let playerX;
 let playerY;
+let playerSize = 50;
+let gameStarted = false;
+let value = 200;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0, 100, 200);
+  background(0, 200, 200);
   playerX = windowWidth/2;
   playerY = windowHeight/2;
 }
 
 function draw() {
-  background(0, 100, 200);
+  fill(value);
+  rect((width/2)-50, (height/2)+200, 100, 50);
 
+  if (gameStarted === true) {
+    background(0, 100, 200);
+  
+    playerMovement();
+    
+    fill(0);
+    noStroke();
+    ellipse(playerX, playerY, playerSize, playerSize);
+  }
+}
+
+function mouseClicked() {
+  if(value === 200) {
+    gameStarted = true;
+  }
+}
+
+function playerMovement() {
   if (keyIsPressed) {
-    if(keyIsDown(87)) {
-      if (playerY !== 0 && playerY !== windowHeight) {
+    if(keyIsDown(87)) { // w
+      if (playerY > 0 + playerSize/2) {
         playerY -= 5;
       }
     }
-    if(keyIsDown(83)) {
-      if (playerY !== 0 && playerY !== windowHeight) {
+    if(keyIsDown(83)) { // s
+      if (playerY < windowHeight - playerSize/2) {
         playerY += 5;
       }
     }
-    if(keyIsDown(68)) {
-      if (playerX !== windowWidth) {
+    if(keyIsDown(68)) { // d
+      if (playerX !== windowWidth - playerSize/2) {
         playerX += 5;
       }
     }
-    if(keyIsDown(65)) {
-      if (playerX !== 0) {
+    if(keyIsDown(65)) { // a
+      if (playerX !== 0 + playerSize/2) {
         playerX -= 5;
       }
     }
   }
-
-  fill(0);
-  noStroke();
-  ellipse(playerX, playerY, 50, 50);
 }
 
 
